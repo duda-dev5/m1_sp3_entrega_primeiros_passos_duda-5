e9f92a1cfde183483258030f55cb032e673a8c0c
Kenzie <eduardaresende90@gmail.com>
2022-10-21 17:08:29 -0300

    entrega da s3-27

---
+++ script.js
@@ -0,0 +1,87 @@
+// //exercício1
+ let variável = 0
+ let xnumero = parseInt(prompt("digite aqui um valor"))
+ let digite = parseInt(prompt("digite aqui outro valor"))
+  if (xnumero>digite) {variável = xnumero-digite
+    alert(`o maior ${xnumero} é o maior número e a diferença entre os dois é ${variável}`)
+
+ }
+
+ else {variável= digite-número 
+  alert(`o maior ${digite} é o maior número e a diferença entre os dois é ${variável}`)
+}
+ //exercício2,
+  let variety = 0
+  let valor = parseInt(prompt("digite um valor"))
+  let valor1 = parseInt(prompt("digite um valor"))
+  if (valor>valor1) {variety = valor-valor1
+ alert(`o número ${valor} é maior que o ${valor1}`)
+}
+
+ //exercício4
+ let variável1= parseInt(prompt("digite um valor"))
+ if (variável1 % 3==0  && variável1 % 5==0 ) {
+     alert( `valor inválido`)
+ }
+  else if (variável1 % 3 ==0) {
+     alert ( `Valor divisível por 3`)
+  }
+
+  else if (variável1 % 5==0 ) {
+  alert (`Valor divisível por 5`)
+  }
+
+ else{ if( variável1%2 ==0) {alert ("É par")}
+  else{ alert (`Ímpar`)}
+ } 
+
+//Exercício7
+ let joy= parseFloat(prompt(" digite um valor em reais"))
+ let cotacao = parseFloat(prompt("digite o valor do dólar"))
+ let resultado = joy*cotacao
+ alert(` O valor ${joy} em dólar é equivalente a ${resultado} tendo R$${cotacao}`)
+
+
+ 
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//exercício 8
+
+
+
+
+// let tipo4 = {newLocal}
+// console.log (resultado)
+// alert("o valor a ser pago é:")
