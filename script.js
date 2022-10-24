2ee480b978d0af6c040ecde40b5e04fabeededcf
Kenzie <eduardaresende90@gmail.com>
2022-10-24 08:36:30 -0300

    tentativa de exercício

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 
  }
 
- else {variável= digite-número 
+ else {variável= digite-xnumero 
   alert(`o maior ${digite} é o maior número e a diferença entre os dois é ${variável}`)
 }
  //exercício2,
@@ -17,14 +17,24 @@
   if (valor>valor1) {variety = valor-valor1
  alert(`o número ${valor} é maior que o ${valor1}`)
 }
-
- //exercício4
+//exercicio3
+ let salario = 0
+ let emprestimo = parseInt(prompt("digite o valor do salário"))
+ let maior = parseInt(prompt("digite novamente um valor"))
+ if(prestacao>emprestimo *0.3){
+    const log = console.log
+   log("emprestimo não concedido")
+}
+ else if(prestacao<emprestimo *0.3){
+   console.log("Empréstimo concedido")
+ }
+ exercício4
  let variável1= parseInt(prompt("digite um valor"))
  if (variável1 % 3==0  && variável1 % 5==0 ) {
-     alert( `valor inválido`)
+  alert( `valor inválido`)
  }
   else if (variável1 % 3 ==0) {
-     alert ( `Valor divisível por 3`)
+  alert ( `Valor divisível por 3`)
   }
 
   else if (variável1 % 5==0 ) {
@@ -35,7 +45,20 @@
   else{ alert (`Ímpar`)}
  } 
 
-//Exercício7
+ //exercicio5
+ let numero= parseInt(prompt("Digite um número de 1 a 7"))
+ if(numero ==1 ) {
+  alert("Domingo")
+ }
+ else (numero==2) {
+alert("segunda-feira")
+
+ }
+ 
+ if(numero % 1==0 && numero % 2==0) 
+ alert (" o numero que digitou não corresponde a um dia da semana")
+
+Exercício7
  let joy= parseFloat(prompt(" digite um valor em reais"))
  let cotacao = parseFloat(prompt("digite o valor do dólar"))
  let resultado = joy*cotacao
